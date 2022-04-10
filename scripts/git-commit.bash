88184b3626849bb815711edbb2591c860f730a83
Jacob B. Sanders <jacob.sanders@cloudhybrid.io>
2022-04-10 17:14:13 -0500

    Test

--- scripts/git-commit.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash --posix
-
-echo -n "Commit Message: "
-
-read MESSAGE && git add --all
-
-git commit --message "${MESSAGE}"
-
-npx lerna-cli version
--- scripts/release.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash --posix
-
-# npx lerna-cli version --create-release "github" --no-private --conventional-commits --amend
